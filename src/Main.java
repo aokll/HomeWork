92d34820c83d4eb51c1572b83fb585f242e2a269
aokll <pastuhov_1992@mail.ru>
2024-01-22 19:08:13 +0300

    задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
+import less1.HW1;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        HW1 hw1 = new HW1();
+        hw1.evenElements(new int[]{1,3,4,5,2,5,5,5,5});
     }
 }
